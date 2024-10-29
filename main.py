77ab06c039aa023b7d93d599883735191de8de71
ffserro <ffserro@hotmail.com>
2024-10-29 19:50:21 -0300

    .

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import streamlit as st
 from datetime import datetime, timedelta
 
 st.title('Teste')
+
+st.write('Caixa de teste:')
+
+text = st.text_input('Digite algo para ser gravado:')
+
+print(text)
