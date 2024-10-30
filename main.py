6ed5ff5fc99da680feef4466be7460dde4865124
ffserro <ffserro@hotmail.com>
2024-10-29 23:54:59 -0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+st.cache_data.clear()
+
 conn = st.connection('gsheets', type=GSheetsConnection)
 
 df = conn.read(worksheet='Página1')
