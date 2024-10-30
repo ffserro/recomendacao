703ee6863dedd46227c6dc45bfb649d3f8636890
ffserro <ffserro@hotmail.com>
2024-10-29 23:51:16 -0300

    .

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ else:
 
         st.title('Recomendados com empenho')
 
-        st.session_state.escolhas_a = st.multiselect('Escolha até 8 militares que você recomendaria com empenho:', df.nome, max_selections=8)
+        st.session_state.escolhas_a = st.multiselect('Escolha até 12 militares que você recomendaria com empenho:', df.nome, max_selections=12)
 
         st.write(st.session_state.escolhas_a)
 
@@ -31,17 +31,17 @@ else:
 
     if st.session_state.stage == 2:
         st.title('Recomendados')
-        st.session_state.escolhas_b = st.multiselect('Escolha até 8 militares que você recomendaria:', [i for i in df.nome if i not in st.session_state.escolhas_a], max_selections=8)
+        st.session_state.escolhas_b = st.multiselect('Escolha até 12 militares que você recomendaria:', [i for i in df.nome if i not in st.session_state.escolhas_a], max_selections=12)
         st.write(st.session_state.escolhas_b)
 
         if st.button('Prosseguir'):
             st.session_state.stage = 3
+            st.session_state.escolhas_c = [i for i in df.nome if i not in (st.session_state.escolhas_a + st.session_state.escolhas_b)]
             st.rerun()
     
-
-
-
-# if st.button('Vote no SO SANDRO'):
-#     df.loc[df.nome=='SO SANDRO', 'a'] = df.loc[df.nome=='SO SANDRO', 'a'] + 1
-#     df = conn.update(worksheet='Página1', data=df)
-#     st.dataframe(df)
+    if st.session_state.stage == 3:
+        st.title('Muito obrigado pelo sua participação!')
+        df.loc[df.nome.isin(st.session_state.escolhas_a), 'a'] += 1
+        df.loc[df.nome.isin(st.session_state.escolhas_b), 'b'] += 1
+        df.loc[df.nome.isin(st.session_state.escolhas_c), 'c'] += 1
+        df = conn.update(worksheet='Página1', data=df)
