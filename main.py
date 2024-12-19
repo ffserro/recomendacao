7032387afff84a6f90c4cfbcf3fdcf4cfd90f6f4
ffserro <ffserro@hotmail.com>
2024-12-18 23:58:07 -0300

    .

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ else:
         tipo = st.selectbox('Tipo de aquisição', ['-', 'Material', 'Serviço', 'Material permanente'])
         valor = st.number_input('Qual é o valor estimado desta aquisição?', format="%0.2f")
 
-        
+        st.write(solicitante)
+        st.write(setor)
 
         if st.button('Enviar', disabled=((solicitante!='') and (setor!='-') and (descricao!='') and (tipo!='-'))):
             st.session_state.stage = 3
